87d2d92b91318175ab9eb7ecc5c6e49146640fcb
Joshua Flowers <joshuatf@gmail.com>
2018-08-23 15:03:55 +0800

    Update version number to 1.1.0

--- woocommerce-bookings-resource-slot-pricing.php
+++ woocommerce-bookings-resource-slot-pricing.php
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WC_BOOKINGS_RESOURCE_SLOT_PRICING', '1.0.0' );
+define( 'WC_BOOKINGS_RESOURCE_SLOT_PRICING', '1.1.0' );
 
 /**
  * The core plugin class that is used to define internationalization,
